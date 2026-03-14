f27cc920a0eecaae7de2f3834b95f446be768308
Santiago <torress631@gmail.com>
2026-03-14 15:58:01 +0100

    Formulario de gastos funcional

--- app/src/main/java/com/santiago/appgastos/MainActivity.kt
+++ app/src/main/java/com/santiago/appgastos/MainActivity.kt
@@ -2,6 +2,7 @@ package com.santiago.appgastos
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,8 +25,18 @@ class MainActivity : AppCompatActivity() {
 
         // accion a realizar cuando le de al boton
         boton.setOnClickListener{
-            // Mostrar mensaje en pantalla
-            Toast.makeText(this,"Boton Pulsado",Toast.LENGTH_SHORT).show()
+            // leer lo que el usuario ingreso
+            val importe = findViewById<EditText>(R.id.etImporte).text.toString()
+            val concepto = findViewById<EditText>(R.id.etConcepto).text.toString()
+
+            // Comprobar que no este vacio
+            if (importe.isNotEmpty() && concepto.isNotEmpty()){
+                // Mostramos mensaje con los datos
+                Toast.makeText(this, "Gastado, $importe en $concepto",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Por favor, rellena todos los campos",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
